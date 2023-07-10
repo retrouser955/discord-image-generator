92332a86dc3fc75f3a7d44f0381940a46a179d32
Retro <htoomyataung382@gmail.com>
2023-07-10 18:51:02 +0630

    push variation generator

---
+++ commands/variation.js
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from "discord.js";
+import { variationGenerator } from "../helpers/generate.js";
+
+export const name = "variation"
+export const description = "Generate 3 images using Dreamlike art Ai model"
+
+export const data = new SlashCommandBuilder()
+.setName(name)
+.setDescription(description)
+.addStringOption((option) => 
+    option.setName("prompt")
+    .setDescription("The prompt you want the AI to generate. Use simple sentences")
+    .setRequired(true)
+)
+.addStringOption((option) => 
+    option.setName('avoid')
+    .setDescription("Things you want to avoid in this generation")
+)
+
+export const run = (client, ctx) => {
+    const prompt = ctx.options.getString("prompt")
+    const negative_prompt = ctx.options.getString("avoid") ?? "blurry"
+
+    variationGenerator(prompt, negative_prompt, ctx)
+}
--- helpers/generate.js
+++ helpers/generate.js
@@ -1,6 +1,7 @@
 import { HfInference } from "@huggingface/inference";
-import { AttachmentBuilder } from "discord.js";
+import { AttachmentBuilder, EmbedBuilder } from "discord.js";
 import chalk from "chalk";
+import joinImages from "join-images";
 
 let inference
 
@@ -10,7 +11,65 @@ console.log(`${chalk.blue("[")} ${chalk.gray("INFO")} ${chalk.blue("]")} : Initi
 
 inference = new HfInference(HF_ACCESS_TOKEN, {
   use_gpu: true,
+  use_cache: false
 })
+
+export async function variationGenerator(prompt, negative_prompt, ctx) {
+  const images = []
+
+  ctx.reply({
+    embeds: [
+      new EmbedBuilder()
+      .setDescription(`<a:loading_color:1089371793468432384> **Generating \`1/3\` images**`)
+      .setColor("Blue")
+    ]
+  })
+
+  for(let i = 0; i < 3; i++) {
+    if(i !== 0) {
+      ctx.editReply({
+        embeds: [
+          new EmbedBuilder()
+          .setDescription(`<a:loading_color:1089371793468432384> **Generating \`${i + 1}/3\` images**`)
+          .setColor("Blue")
+        ]
+      })
+    }
+
+    const image = await inference.textToImage({
+      inputs: prompt,
+      parameters: {
+        negative_prompt: negative_prompt ?? "blurry",
+        width: 800,
+        height: 1000
+      },
+      model: "dreamlike-art/dreamlike-anime-1.0"
+    })
+
+    images.push(Buffer.from(await image.arrayBuffer()))
+  }
+  
+  ctx.editReply({
+    embeds: [
+      new EmbedBuilder()
+      .setDescription(`<:tick:1089371868420653066> **Generated your prompt!**`)
+      .setColor("Blue")
+    ]
+  })
+
+  const sharp = await joinImages.joinImages(images, {
+    direction: "horizontal"
+  })
+
+  const buffer = await sharp.toFormat("png").toBuffer()
+
+  const attachment = new AttachmentBuilder(buffer, {name:"image.png"})
+
+  ctx.followUp({
+    content: ctx.user.toString(),
+    files: [attachment]
+  })
+}
 
 export async function generatePrompt(prompt, model, negative_prompt, name) {
   const image = await inference.textToImage({
@@ -33,5 +92,6 @@ setInterval(() => {
   console.log(`${chalk.blue("[")} ${chalk.gray("INFO")} ${chalk.blue("]")} : Resetting Hugging Face Client`)
   inference = new HfInference(HF_ACCESS_TOKEN, {
     use_gpu: true,
+    use_cache: false
   });
 }, 1 * 60 * 60 * 1000)
